048cb989ea741d4071038e8d0c0c295f5bad08c3
Anthony DeBarros <adebarros@gmail.com>
2012-05-28 12:07:06 -0400

    Switch to Google fonts; many style updates.

--- header.php
+++ header.php
@@ -9,15 +9,20 @@
 	<meta name="resource-type" content="document" />
 	<meta http-equiv="content-type" content="text/html; charset=US-ASCII" />
 	<meta http-equiv="content-language" content="en-us" />
+	<meta name="google-site-verification" content="3cXq7qpM2caPBH74yvmYJYdTIw18jO5ESgpf1JNMRTw" />
 	<meta http-equiv="Content-Type" content="text/html; ?>; charset=UTF-8; ?>" />	
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
 
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="/xmlrpc.php" />
         <link rel="shortcut icon" href="/wp-content/themes/goshen/favicon.ico" />
+
+        <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 
 	<?php wp_get_archives('type=monthly&format=link'); ?>
 	<?php //comments_popup_script(); // off by default ?>
--- sidebar.php
+++ sidebar.php
@@ -6,47 +6,46 @@
     <?php bloginfo('description'); ?>
 </div>
 <br/>
-    <h2><b>ABOUT</b></h2>
+<h2>Back catalog</h2>
+<br/>
+    <h3>ABOUT</h3>
         <ul>
         <li>- <a href="http://www.anthonydebarros.com/resume/" title="Resume">Resume</a></li>
         <li>- <a href="http://www.anthonydebarros.com/about/" title="Bio">Bio</a></li>
         <li>- <a href="http://www.linkedin.com/in/debarros1">LinkedIn</a></li>
         </ul>
-    <h2><b>CODING PROJECTS</b></h2>
+    <h3>CODING PROJECTS</h3>
         <ul>
         <li>- <a href="http://www.anthonydb.com/words/" title="Words on Art" target="_blank">Words on Art (Django)</a></li>
         <li>- <a href="http://www.anthonydb.com/apis/books" title="Book List API" target="_blank">Book List API example (Django)</a></li>
         <li>- <a href="https://github.com/AnthonyDB/goshen-wordpress-theme" title="Goshen WordPress Theme" target="_blank">Goshen theme (WordPress)</a></li>
         <li>- <a href="http://github.com/AnthonyDB">Github</a></li>
         </ul>        
-    <h2><b>JOURNALISM</b></h2>
+    <h3>JOURNALISM</h3>
         <ul>
         <li>- <a href="http://www.anthonydebarros.com/clips/investigative/" title="Investigative">Investigative data journalism</a></li>
         <li>- <a href="http://www.anthonydebarros.com/clips/interactivity/" title="News applications">News applications</a></li>
         <li>- <a href="http://www.anthonydebarros.com/clips/" title="Clips">More clips</a></li>
         <li>- <a href="http://www.anthonydebarros.com/speaking-training/" title="Speaking & training">Speaking & Training</a></li>
         </ul>        
-    <h2><b>CONTACT, FOLLOW</b></h2>
+    <h3>CONTACT &amp; FOLLOW</h3>
         <ul>
         <li>- <a href="http://www.anthonydebarros.com/contact/" title="Contact">Contact</a></li>
         <li>- <a href="http://www.twitter.com/AnthonyDB">Twitter</a></li>
         <li>- <a href="http://pinboard.in/u:anthonydb">Pinboard</a></li>
         <li>- <a href="http://letusgo.tumblr.com/">Tumblr</a></li>
         </ul>
-    <g:plusone size="medium" href="http://www.anthonydebarros.com"></g:plusone>
-<br/>
-<br/>
-<h2><b>Subscribe</b></h2>
-<br/>
-    <a href="<?php bloginfo('atom_url'); ?>" title="<?php _e('Atom'); ?>">
-    <img src="/wp-content/themes/goshen/img/feed-icon.png" alt="Atom" title="Atom" /> Atom</a><br/>
-    <a href="<?php bloginfo('rss_url'); ?>" title="<?php _e('RSS .92'); ?>">
-    <img src="/wp-content/themes/goshen/img/feed-icon.png" alt="RSS .92" title="RSS .92" /> RSS .92</a><br/>
-    <a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('RSS 2.0'); ?>">
-    <img src="/wp-content/themes/goshen/img/feed-icon.png" alt="RSS 2.0" title="RSS 2.0" /> RSS 2.0</a><br/>
-<br/>
+<h2>Subscribe</h2>
+<ul>
+    <li><a href="<?php bloginfo('atom_url'); ?>" title="<?php _e('Atom'); ?>">
+    <img src="/wp-content/themes/goshen/img/feed-icon.png" alt="Atom" title="Atom" /> Atom</a></li>
+    <li><a href="<?php bloginfo('rss_url'); ?>" title="<?php _e('RSS .92'); ?>">
+    <img src="/wp-content/themes/goshen/img/feed-icon.png" alt="RSS .92" title="RSS .92" /> RSS .92</a></li>
+    <li><a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('RSS 2.0'); ?>">
+    <img src="/wp-content/themes/goshen/img/feed-icon.png" alt="RSS 2.0" title="RSS 2.0" /> RSS 2.0</a></li>
+</ul>
 <?php if(function_exists('stats_get_csv')) : ?>
-	<h2><b>Most-viewed posts</b></h2>
+	<h2>Most-viewed</h2>
 	<ul id="popularposts">
 	<?php
 	    $top_posts = stats_get_csv('postviews', 'days=60&limit=13');
@@ -61,36 +60,36 @@
 	</ul>
 <?php endif; ?>
 
-<h2><b>microblog</b></h2>
+<h2>Twitter</h2>
 <?php twitter_messages("AnthonyDB", 5, true, true, '>><br />', true, false, false); ?>
 
-<h2><b>data journalism helps</b></h2>
-<br/>
-<a href="http://github.com/onyxfish/csvkit">csvkit</a><br />
-<a href="http://www.shancarter.com/data_converter/index.html">Mr. Data Converter</a><br />
-<a href="http://scraperwiki.com/help/tutorials/">ScraperWiki Tutorials</a><br />
-<br />
-
+<h2>Helpful things</h2>
+<ul>
+<li><a href="http://github.com/onyxfish/csvkit">csvkit</a></li>
+<li><a href="http://www.shancarter.com/data_converter/index.html">Mr. Data Converter</a></li>
+<li><a href="http://scraperwiki.com/help/tutorials/">ScraperWiki Tutorials</a></li>
+</ul>
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar") ) : ?>
 
 <?php endif; ?>
 
-<h2><b><?php _e('Archives'); ?></b></h2>
+<h2><?php _e('Archives'); ?></h2>
 
 <ul>
 <?php wp_get_archives('type=monthly&show_post_count=1'); ?>
 </ul>
 
-<h2><b><?php _e('Categories'); ?></b></h2>
+<h2><?php _e('Categories'); ?></h2>
 
 <ul>
 <?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
 </ul>
 
-<h2><b><?php _e('Search'); ?></b></h2>
+<h2><?php _e('Search'); ?></h2>
 <br />
 <?php include(TEMPLATEPATH.'/searchform.php'); ?>
 <br />
-
+<br/>
+    <g:plusone size="medium" href="http://www.anthonydebarros.com"></g:plusone>
 
 </div>
